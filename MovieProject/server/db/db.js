245cab63be371c08eb3dca8e7282baf85376383a
Roshan <123092070+roshan773@users.noreply.github.com>
2025-08-09 13:23:39 +0530

    Delete MovieProject directory

--- MovieProject/server/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-async function connectToDb() {
-    await mongoose.connect(process.env.MONGO_URL)
-    console.log("Connected to MongoDB>>>>>>>");
-}
-
-module.exports = connectToDb;
